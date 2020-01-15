c3c4d9df08deec17ba9b25b90fb1ea5e280885e2
James Meyer <Jamesmeyer75@gmail>
2020-01-15 16:58:16 -0600

    new

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     $(".header").hide().fadeIn(500);
-    $("contact-me-box").hide().fadeIn(1000);
+    $(".contact-me-box").hide().fadeIn(1000);
 });
 
 // link to other pages
 $(".about-me").on("click", function () {
-    $("contact-me-box").hide();
+    $(".contact-me-box").hide();
     window.location.href = "../index.html";
 });
 
 $(".port-text").on("click", function () {
-    $("contact-me-box").hide();
+    $(".contact-me-box").hide();
     window.location.href = "./portfolio.html";
 });
