63f82edb4c9ad8dfec63c4dd774553dc940fd577
James Meyer <Jamesmeyer75@gmail>
2020-01-15 15:27:03 -0600

    new

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 // link to other pages
 $(".about-me").on("click", function () {
     $("#show-carousel").hide();
-    window.location.href = "../../index.html";
+    window.location.href = "../../../index.html";
 });
 
 $(".contact").on("click", function () {
